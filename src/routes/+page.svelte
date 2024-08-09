9dc94f7b6f94d9c64475fed2a717642ba5941c88
Bryan Casanelli <bryancasanelli@gmail.com>
2024-08-09 02:49:35 -0400

    Picture added.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { t } from '$lib/translations/translations';
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="flex min-h-screen items-center justify-center bg-zinc-800">
 	<div class="text-center">
+		<Avatar src="images/bryancasanelli.jpeg" class="w-48 rounded-full mx-auto" />
 		<div class="flex items-end justify-center">
-			<h1 class="noto-serif-regular mb-4 text-7xl text-white">Bryan Casanelli</h1>
+			<h1 class="noto-serif-regular mb-4 mt-4 text-7xl text-white">Bryan Casanelli</h1>
 		</div>
 		<p class="text-1xl text-white">{$t('common.under_construction')}</p>
 	</div>
